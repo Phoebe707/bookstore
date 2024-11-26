b83ba440f417775610287271a3a610ecbb349b05
Devin Liang <devinliang@gmail.com>
2024-11-26 16:27:48 +0800

    update

--- book/views.py
+++ book/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+from book.models import book
 # Create your views here.
 
 def index(request):
     # return HttpResponse("書名列表...")
-    return render(request, 'index.html')
+    bb = book.objects.all()
+    return render(request, 'index.html', locals())
 
 def harrypotter(request):
     # return HttpResponse("我是哈利波特")
